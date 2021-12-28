c5657226aa868635bec8a86d154694df175fe7d4
Window5000 <hjalte@toppenafverden.dk>
2021-12-28 17:12:51 +0100

    wadawdwad

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('interactionCreate', async interaction => {
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreated at ${interaction.guild.createdAt}`);
 	} else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	}
