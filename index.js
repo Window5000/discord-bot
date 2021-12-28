84c07cd7b8ceedcc977d432088f0ebac735e689d
Window5000 <hjalte@toppenafverden.dk>
2021-12-28 16:17:07 +0100

    test 4

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const Discord = require("discord.js");
-console.log("hello world")
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+
+client.login("OTI1MzUwMjg4MDcwNzAxMTE4.Ycr1nA.5UFbpGZ8cEYiEgOT7NQteV_Z7dM")
