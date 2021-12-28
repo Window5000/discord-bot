99daa323cf1ef46e44e8ba7a585e84efe3ba347b
Window5000 <hjalte@toppenafverden.dk>
2021-12-28 16:56:02 +0100

    commands

--- index.js
+++ index.js
@@ -9,5 +9,17 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
