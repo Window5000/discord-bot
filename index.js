c32b3f67740fd3db22860d70e89e1279243f430d
Window5000 <hjalte@toppenafverden.dk>
2021-12-28 17:24:06 +0100

    LKWANFWANF

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('interactionCreate', async interaction => {
 	} else if (commandName === 'server') {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreated at: ${interaction.guild.createdAt}`);
 	} else if (commandName === 'user') {
-		await interaction.reply(`Your name: ${interaction.user.tag}`);
+		await interaction.reply(`Your username: ${interaction.user.tag}`);
 	}
 });
 
